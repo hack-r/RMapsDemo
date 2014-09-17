e168511102c27631699acae2b00d102b28c10644
Jason Miller <jmillerlaureate@gmail.com>
2014-09-17 17:03:11 -0400

    .

---
+++ RMapsDemo_Functions.R
@@ -0,0 +1,13 @@
+## File: RMapsDemo_Functions.R
+## Description: Function file for demonstrating GIS/maps in R
+## Copyright: (c) 2014, Hack-R.com (Jason D. Miller)
+
+
+# Packages ----------------------------------------------------------------
+require(googleVis)
+
+# Functions ---------------------------------------------------------------
+# I don't think we'll need any custom functions for this one 
+
+
+
